304c5774bbdb428b95142e72095a9ae4b6c58a8c
Md.Mazadul Islam <secretmind456@gmail.com>
2026-01-07 16:23:30 +0600

    matrix.js

--- Javascript/matrix.js
+++ Javascript/matrix.js
@@ -22,7 +22,7 @@ function draw() {
     ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     
-    ctx.fillStyle = "rgba(47, 65, 47, 1)";
+    ctx.fillStyle = "rgba(3, 86, 3, 1)";
     ctx.font = fontSize + "px monospace";
     
     for (let i = 0; i < drops.length; i++) {
